6ec6cdb184ed30e66b09f2cb133a6215235b7edc
ZeroSTF <35609287+ZeroSTF@users.noreply.github.com>
2025-01-25 00:31:11 +0100

    piece image cacheing

--- src/main/java/tn/zeros/zchess/ui/util/AssetLoader.java
+++ src/main/java/tn/zeros/zchess/ui/util/AssetLoader.java
@@ -4,19 +4,24 @@ package tn.zeros.zchess.ui.util;
 import javafx.scene.image.Image;
 import tn.zeros.zchess.core.model.Piece;
 
+import java.util.EnumMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class AssetLoader {
     private static final String PIECE_ASSET_PATH = "/pieces/%s.png";
+    private static final Map<Piece, Image> PIECE_CACHE = new EnumMap<>(Piece.class);
 
     public static Image getPieceImage(Piece piece) {
-        try {
-            String path = String.format(PIECE_ASSET_PATH, piece.name());
-            return new Image(Objects.requireNonNull(
-                    AssetLoader.class.getResourceAsStream(path)
-            ));
-        } catch (Exception e) {
-            return null;
-        }
+        return PIECE_CACHE.computeIfAbsent(piece, p -> {
+            String path = String.format(PIECE_ASSET_PATH, p.name());
+            try {
+                return new Image(Objects.requireNonNull(
+                        AssetLoader.class.getResourceAsStream(path)
+                ));
+            } catch (Exception e) {
+                return null;
+            }
+        });
     }
 }
